2654cd324d3175859c178e6571b0ad60e97604f8
miranda_daniel <miranda_daniel@hotmail.com>
2016-06-09 13:08:55 -0300

    septimo commit

--- app/Config/database.php
+++ app/Config/database.php
@@ -74,7 +74,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => '',
+		'database' => 'seguridad',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
